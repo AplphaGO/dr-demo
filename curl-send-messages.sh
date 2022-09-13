cf547574a3dce385d0c6b786e3b48f2a57db6ef7
Daniel Parkes <dparkes@redhat.com>
2022-09-13 13:29:29 +0200

    fixes

--- curl-send-messages.sh
+++ curl-send-messages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-SITE=cluster1
+SITE1=cluster1
+SITE2=cluster2
 HOOK1=http://rocket-chat-rocketchat.apps.bos2.chris.ocs.ninja/hooks/7LiiXS7EEF5id8TGS/vFHQpzEoKFdfAuock2S7zN5vZXccnwwzXFHB8JjsxnubZCbK
 HOOK2=http://rocket-chat-rocketchat.apps.bos3.chris.ocs.ninja/hooks/7LiiXS7EEF5id8TGS/vFHQpzEoKFdfAuock2S7zN5vZXccnwwzXFHB8JjsxnubZCbK
 TIME=10
@@ -7,6 +8,6 @@ export NUMBER=1
 while true ; do 
   sleep $TIME
   export NUMBER=$((NUMBER+1))
-  curl -X POST -H 'Content-Type: application/json' --data '{"text":"Sending Message to test DR POC","attachments":[{"title":"'"Site: ${SITE}"'","text":"'"DR message ${NUMBER}"'"}]}' ${HOOK1}
-  curl -X POST -H 'Content-Type: application/json' --data '{"text":"Sending Message to test DR POC","attachments":[{"title":"'"Site: ${SITE}"'","text":"'"DR message ${NUMBER}"'"}]}' ${HOOK2}
+  curl -X POST -H 'Content-Type: application/json' --data '{"text":"Sending Message to test DR POC","attachments":[{"title":"'"Site: ${SITE1}"'","text":"'"DR message ${NUMBER}"'"}]}' ${HOOK1}
+  curl -X POST -H 'Content-Type: application/json' --data '{"text":"Sending Message to test DR POC","attachments":[{"title":"'"Site: ${SITE2}"'","text":"'"DR message ${NUMBER}"'"}]}' ${HOOK2}
 done
