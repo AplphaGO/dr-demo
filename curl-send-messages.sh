3f99339c772294e3b5933c02a7f620efb3a8a1d8
Daniel Parkes <dparkes@redhat.com>
2022-09-16 12:51:21 +0200

    fixes

--- curl-send-messages.sh
+++ curl-send-messages.sh
@@ -8,6 +8,6 @@ export NUMBER=1
 while true ; do 
   sleep $TIME
   export NUMBER=$((NUMBER+1))
-  curl -X POST -H 'Content-Type: application/json' --data '{"text":"Sending Message to test DR POC","attachments":[{"title":"'"Site: ${SITE1}"'","text":"'"DR message ${NUMBER}"'"}]}' ${HOOK1}
-  curl -X POST -H 'Content-Type: application/json' --data '{"text":"Sending Message to test DR POC","attachments":[{"title":"'"Site: ${SITE2}"'","text":"'"DR message ${NUMBER}"'"}]}' ${HOOK2}
+  curl -X POST -H 'Content-Type: application/json' --data '{"username": "cluster1","icon_emoji": ":alien:","text":"Sending Message to test DR POC. '" $(date)"'","attachments":[{"title":"'"Site: ${SITE1}"'","text":"'"DR message ${NUMBER}"'","color":"#0ff004"}]}' ${HOOK1}
+  curl -X POST -H 'Content-Type: application/json' --data '{"username": "cluster2","icon_emoji": ":robot:","text":"Sending Message to test DR POC. '" $(date)"'","attachments":[{"title":"'"Site: ${SITE2}"'","title_link":"https://rocket.chat","text":"'"DR message ${NUMBER}"'","color":"#f06f04"}]}' ${HOOK2}
 done
