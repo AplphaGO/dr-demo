e4b628dc72a7b4043ddbfce8a08c84d1840df170
Daniel Parkes <dparkes@redhat.com>
2022-09-21 23:10:23 +0200

    fixes

--- rocket-chat/acm/failover_to_site2.sh
+++ rocket-chat/acm/failover_to_site2.sh
@@ -1,3 +1,2 @@
-sleep 15
     oc patch drpc rocketchat-placement-1-drpc  --type json -p "[{'op': 'add', 'path': '/spec/failoverCluster', 'value': "cluster2"}]" -n rocketchat
     oc patch drpc rocketchat-placement-1-drpc  --type json -p "[{'op': 'add', 'path': '/spec/action', 'value': 'Failover'}]" -n rocketchat
