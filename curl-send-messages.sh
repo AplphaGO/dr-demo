aa4ae2d9c059bb1ec1d29517b302b1ef4742123a
Daniel Parkes <dparkes@redhat.com>
2022-09-27 21:48:16 +0200

    fixes

--- curl-send-messages.sh
+++ curl-send-messages.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 SITE1=cluster1
 SITE2=cluster2
-HOOK1=http://rocket-chat-rocketchat.apps.bos2.chris.ocs.ninja/hooks/XN8Hw6WwAk8EJyBGp/RmmmFddWzmbfX6KST4qamt9dzxnQofK3L4WBngjqRMb8jJYo
-HOOK2=http://rocket-chat-rocketchat.apps.bos3.chris.ocs.ninja/hooks/XN8Hw6WwAk8EJyBGp/RmmmFddWzmbfX6KST4qamt9dzxnQofK3L4WBngjqRMb8jJYo
+TOKEN='ZZqznidqPwBGPq2eT/viLdFfgyomjNYubjgrQmaTKWLADv5Cjjpp7Yh7o9aLjDuKeR'
+HOOK1=http://rocket-chat-rocketchat.apps.bos2.chris.ocs.ninja/hooks/${TOKEN}
+HOOK2=http://rocket-chat-rocketchat.apps.bos3.chris.ocs.ninja/hooks/${TOKEN}
 TIME=10
-export NUMBER=1 
+export NUMBER=0 
 while true ; do 
   sleep $TIME
   export NUMBER=$((NUMBER+1))
-  curl -X POST -H 'Content-Type: application/json' --data '{"username": "cluster1","icon_emoji": ":alien:","text":"Sending Message to test DR POC. '" $(date)"'","attachments":[{"title":"'"Site: ${SITE1}"'","text":"'"DR message ${NUMBER}"'","color":"#0ff004"}]}' ${HOOK1}
-  curl -X POST -H 'Content-Type: application/json' --data '{"username": "cluster2","icon_emoji": ":robot:","text":"Sending Message to test DR POC. '" $(date)"'","attachments":[{"title":"'"Site: ${SITE2}"'","title_link":"https://rocket.chat","text":"'"DR message ${NUMBER}"'","color":"#f06f04"}]}' ${HOOK2}
+  curl -X POST -H 'Content-Type: application/json' --data '{"username": "CLUSTER1","icon_emoji": ":alien:","text":"Sending Message to test DR POC. '" $(date)"'","attachments":[{"title":"'"Site: ${SITE1}"'","text":"'"DR message ${NUMBER}"'","color":"#0ff004"}]}' ${HOOK1}
+  curl -X POST -H 'Content-Type: application/json' --data '{"username": "CLUSTER2","icon_emoji": ":robot:","text":"Sending Message to test DR POC. '" $(date)"'","attachments":[{"title":"'"Site: ${SITE2}"'","title_link":"https://rocket.chat","text":"'"DR message ${NUMBER}"'","color":"#f06f04"}]}' ${HOOK2}
 done
